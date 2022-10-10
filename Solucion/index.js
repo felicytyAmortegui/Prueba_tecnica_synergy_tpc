9dc6f790eb7d4fcc7280621a283621570d4f61da
felicytyAmortegui <91999614+felicytyAmortegui@users.noreply.github.com>
2022-10-10 11:58:16 -0500

    Fourth commit - push example

--- Solucion/index.js
+++ Solucion/index.js
@@ -1,34 +1,55 @@
 
-const urlListaElectrodomesticos= `https://electrics.azurewebsites.net/Electrodomestico/ListarElectrodomesticos?dataOwner=21fafec5-754f-4ee5-b668-acd472316765`;
+const boton = document.getElementById('boton_datos');
 
+const boton_enviar = document.getElementById('boton_enviar')
 
-const lista = [
+const urlLista= `https://electrics.azurewebsites.net/Electrodomestico/ListarElectrodomesticos?dataOwner=21fafec5-754f-4ee5-b668-acd472316765`;
+
+const listaProductos = [
     {
-    nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
-    garantia:'1 año',
-    modelo:'QN43Q65BAKXZL',
-    tecnologia:'QLED',
-    dimensiones:'43 pulgadas'},
+     nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
+     caracteristicas:{
+        dimensiones:'43 pulgadas',
+        tecnologia:'QLED',
+        modelo:'QN43Q65BAKXZL',
+        refresh:'60Hz',
+        garantia:'12 meses' 
+     }   
+    },
+
     {
         nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
-        garantia:'1 año',
-        modelo:'QN43Q65BAKXZL',
-        tecnologia:'QLED',
-        dimensiones:'43 pulgadas'}
-    ];
-    
-    fetch(urlListaElectrodomesticos, {
-        method: "POST",
-        body: JSON.stringify(lista),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-    .then(response => response.json()) 
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
+        caracteristicas:{
+           dimensiones:'43 pulgadas',
+           tecnologia:'QLED',
+           modelo:'QN43Q65BAKXZL',
+           refresh:'60Hz',
+           garantia:'12 meses' 
+        }   
+       }
+]
 
+boton.addEventListener('click',()=>{
 
-
-    
-    fetch(urlListaElectrodomesticos)
-    .then(res => console.log(res.data))
+    //Llamada
+    fetch(urlLista)
+    .then(res => res.json())
+    .then(data => console.log(data))
     .catch(er=> console.log(er));
+
+});
+
+boton_enviar.addEventListener('click',()=>{
+
+
+    fetch(urlLista,
+        {
+            method:'POST',
+            body:JSON.stringify(listaProductos),
+            headers:{'Content-type':'application/json'}
+        })
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+
+
+    })

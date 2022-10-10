5d0c95a2c68ac448c64b84b199d92b85fd108f33
felicytyAmortegui <91999614+felicytyAmortegui@users.noreply.github.com>
2022-10-10 12:51:56 -0500

    fifth commit - post-example

--- Solucion/index.js
+++ Solucion/index.js
@@ -4,30 +4,40 @@ const boton = document.getElementById('boton_datos');
 const boton_enviar = document.getElementById('boton_enviar')
 
 const urlLista= `https://electrics.azurewebsites.net/Electrodomestico/ListarElectrodomesticos?dataOwner=21fafec5-754f-4ee5-b668-acd472316765`;
+const urlCrearElectrodomestico = 'https://electrics.azurewebsites.net/Electrodomestico/CrearElectrodomestico?dataOwner=21fafec5-754f-4ee5-b668-acd472316765'
 
-const listaProductos = [
-    {
-     nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
-     caracteristicas:{
-        dimensiones:'43 pulgadas',
-        tecnologia:'QLED',
+const listaProductos =
+       {
+        nombre:'Televisor LG 77 pulgadas OLED 4K Ultra HD Smart TV',
+        alto:'43 pulgadas',
+        ancho:'43 pulgadas',
+        largo:'43 pulgadas',
+        descripcion:'Televisor LG 77 pulgadas OLED 4K Ultra HD Smart TV',
+        precio:'1.699.000',
+        idCategoria:'QLED',
+        idMarca:'Samsung',
         modelo:'QN43Q65BAKXZL',
-        refresh:'60Hz',
-        garantia:'12 meses' 
-     }   
-    },
+        tiempoGarantia:'12 meses',
+        unidadesDisponibles:19
+       };
 
-    {
-        nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
-        caracteristicas:{
-           dimensiones:'43 pulgadas',
-           tecnologia:'QLED',
-           modelo:'QN43Q65BAKXZL',
-           refresh:'60Hz',
-           garantia:'12 meses' 
-        }   
-       }
-]
+
+
+const listaProductos2 ={
+        nombre:'Televisor LG 77 pulgadas OLED 4K Ultra HD Smart TV',
+        alto:43,
+        ancho:43,
+        largo:43,
+        descripcion:'Televisor LG 77 pulgadas OLED 4K Ultra HD Smart TV',
+        precio:1699000,
+        idCategoria:'QLED',
+        idMarca:199,
+        modelo:'QN43Q65BAKXZL',
+        tiempoGarantia:1,
+        unidadesDisponibles:19
+    };
+
+
 
 boton.addEventListener('click',()=>{
 
@@ -39,17 +49,11 @@ boton.addEventListener('click',()=>{
 
 });
 
-boton_enviar.addEventListener('click',()=>{
-
-
-    fetch(urlLista,
-        {
-            method:'POST',
-            body:JSON.stringify(listaProductos),
-            headers:{'Content-type':'application/json'}
-        })
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-
-
+fetch(urlCrearElectrodomestico,
+    {
+        method:'POST',
+        body:JSON.stringify(listaProductos2),
+        headers:{'Content-type':'application/json'}
     })
+    .then(res=>res.json())
+    .then(data=>console.log(data))

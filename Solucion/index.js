f4d317819403827cbdc2cce63b8a7e82a8d0a3b2
felicytyAmortegui <91999614+felicytyAmortegui@users.noreply.github.com>
2022-10-10 10:56:13 -0500

    third commit - fetch

--- Solucion/index.js
+++ Solucion/index.js
@@ -1,13 +1,34 @@
-const llamada = async (url)=>{
 
-    const i = await fetch(url)
-    .then(res => res)
-    .catch(er=>er);
-
-    return i
-}
-
-const urlListaElectrodomesticos= `https://electrics.azurewebsites.net/Electrodomestico/ListarElectrodomesticos`;
+const urlListaElectrodomesticos= `https://electrics.azurewebsites.net/Electrodomestico/ListarElectrodomesticos?dataOwner=21fafec5-754f-4ee5-b668-acd472316765`;
 
 
-console.log(llamada(urlListaElectrodomesticos)); 
+const lista = [
+    {
+    nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
+    garantia:'1 año',
+    modelo:'QN43Q65BAKXZL',
+    tecnologia:'QLED',
+    dimensiones:'43 pulgadas'},
+    {
+        nombre:'Televisor Samsung 43 pulgadas QLED 4K Ultra HD Smart TV',
+        garantia:'1 año',
+        modelo:'QN43Q65BAKXZL',
+        tecnologia:'QLED',
+        dimensiones:'43 pulgadas'}
+    ];
+    
+    fetch(urlListaElectrodomesticos, {
+        method: "POST",
+        body: JSON.stringify(lista),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+
+
+
+    
+    fetch(urlListaElectrodomesticos)
+    .then(res => console.log(res.data))
+    .catch(er=> console.log(er));
